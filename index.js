425d9b83dc333f2d2623ab9c85cc0f633f7e393d
Mark <Marcusa.helms@gmail.com>
2020-08-19 17:34:29 -0500

    more CSS revisions

--- index.js
+++ index.js
@@ -46,7 +46,18 @@ for (let item of items) {
 }
 
 
+/*              N A V    B A R    S C R O L L                    */
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 
 
