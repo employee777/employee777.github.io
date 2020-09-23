5298b73f949e6337302adcbc1aea2570f3396617
Mark <Marcusa.helms@gmail.com>
2020-09-23 13:27:10 -0500

    almost finished O_O

--- JavaScript/Modal.js
+++ JavaScript/Modal.js
@@ -1,59 +1,32 @@
 /*            Modal           */
 
-// GAME
+var MessageBtn = document.getElementById("MessageModal");
+var ModalMessage = document.getElementById("MessageMe");
+var GameBtn = document.getElementById("GameModal");
+var ModalGame = document.getElementById("Game");
+var Modal1 = document.getElementsByClassName("modal")[0];
+var Modal2 = document.getElementsByClassName("modal")[1];
 
 
+GameBtn.onclick = function() {
+  ModalGame.style.display = "flex";
+}
+MessageBtn.onclick = function() {
+  ModalMessage.style.display = "flex";
+}
 
-
-
-//document.querySelector('.myClassName').id[0]
-
-//document.querySelector('.myClassName').id[0]
-
-//.style.display = "block";
-
-
-// CLOSE
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  (ModalGame.style.display = "none") || (ModalMessageMe.style.display = "none");
+function MessageClose() {
+  ModalMessage.style.display = "none";
+  ModalGame.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target === ModalMessageMe) {
-    ModalMessageMe.style.display = "none";
+  if (event.target === Modal2) {
+    ModalMessage.style.display = "none";
   }
-}
-
-window.onclick = function(event) {
-  if (event.target === ModalGame) {
+  if (event.target === Modal1) {
     ModalGame.style.display = "none";
   }
-}
-
-
-
-var ModalGame = document.getElementById("Game");
-// Get the button that opens the modal
-var GameBtn = document.getElementById("GameModal");
-// When the user clicks on the button, open the modal
-GameBtn.onclick = function() {
-  ModalGame.style.display = "block";
-}
-// When the user clicks anywhere outside of the modal, close it
-
-
-// MESSAGE
-
-var ModalMessageMe = document.getElementById("MessageMe");
-// Get the button that opens the modal
-var MessageBtn = document.getElementById("MessageMeModal");
-// When the user clicks on the button, open the modal
-MessageBtn.onclick = function() {
-  ModalMessageMe.style.display = "block";
 }
 
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,37 +1,3 @@
-
-
-/*                       M E N U                         */
-
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
-const items = document.querySelectorAll(".item");
-
-/* Toggle mobile menu */
-function toggleMenu() {
-  if (menu.classList.contains("active")) {
-    menu.classList.remove("active");
-    toggle.querySelector("a").innerHTML = "<i class='fas fa-link'></i>";
-  } else {
-    menu.classList.add("active");
-    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-  }
-}
-
-function toggleMenu() {
-  var x = document.getElementsByClassName("link");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-/* Close Submenu From Anywhere */
-
-
-/* Event Listeners */
-/*                   E N D   O F   M E N U                      */
-
-
 /*                 N A V    B A R    S C R O L L                    */
 
 var prevScrollpos = window.pageYOffset;
